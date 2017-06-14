846934e31afe7cfbd4f33795bcb761b306b81c6a
adamesh <adameshh@gmail.com>
2017-06-14 17:59:31 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
+require 'SQLite3'
+require 'pry'
+
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(info_hash)
+    @id = info_hash[:id]
+    @name = info_hash[:name]
+    @type = info_hash[:type]
+    @@db = info_hash[:db]
+  end
+
+  def self.save(name, type, db)
+    command = "INSERT INTO pokemon (name, type) VALUES ('#{name}', '#{type}');"
+    db.execute(command)
+  end
+
+  def self.find(id, db)
+    command = "SELECT * FROM pokemon WHERE id = '#{id}'"
+    poke_stats = db.execute(command).first
+    new_pokemon = Pokemon.new(
+      {id:poke_stats[0],
+      name:poke_stats[1],
+      type:poke_stats[2]}
+      )
+  end
+
+
 end
