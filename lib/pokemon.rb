c39a4616f17cca25083bc75b149e6106bf5c4d1a
adamesh <adameshh@gmail.com>
2017-06-22 17:52:11 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,12 +3,13 @@ require 'pry'
 
 
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :hp, :db
 
   def initialize(info_hash)
     @id = info_hash[:id]
     @name = info_hash[:name]
     @type = info_hash[:type]
+    @hp = info_hash[:hp]
     @@db = info_hash[:db]
   end
 
@@ -23,8 +24,14 @@ class Pokemon
     new_pokemon = Pokemon.new(
       {id:poke_stats[0],
       name:poke_stats[1],
-      type:poke_stats[2]}
+      type:poke_stats[2],
+      hp:poke_stats[3]}
       )
+  end
+
+  def alter_hp(hp, db)
+    command = "UPDATE pokemon SET hp = #{hp} WHERE id = '#{self.id}'"
+    db.execute(command)
   end
 
 
